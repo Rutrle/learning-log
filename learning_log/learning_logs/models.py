9a8247c83d7b1c92f0bb48da9d8c45341f2ba3cd
Jakub <rutrle@seznam.cz>
2021-01-26 23:57:29 +0100

    Topic model

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    "a topic which the user is learning about"
+    text = models.CharField(max_length=200)
+    date_added = models.DateTime(auto_now_add=True)
+
+
+    def _str_(self):
+        '''return string representation of model'''
+        return self.text
