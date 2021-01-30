40cddcf06065c27c595444228ad2294354c02188
Jakub <rutrle@seznam.cz>
2021-01-30 12:51:31 +0100

    exercise 18-6

--- exercises/pizzeria/pizzas/urls.py
+++ exercises/pizzeria/pizzas/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
+app_name='pizzas'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pizzas.urls'))
+    path('',views.index_view, name='index'  )
 ]
