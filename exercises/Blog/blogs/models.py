5f8f7559a5493d0ac1e022bd44e6b6467b33a199
Jakub <rutrle@seznam.cz>
2021-02-02 22:03:29 +0100

    blogs home page

--- exercises/Blog/blogs/models.py
+++ exercises/Blog/blogs/models.py
@@ -6,4 +6,7 @@ class Blogpost(models.Model):
     '''model for blogpost'''
     title = models.CharField(max_length=200)
     text = models.TextField()
-    date =models.DateTimeField()
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
--- exercises/Blog/blogs/views.py
+++ exercises/Blog/blogs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Blogpost
 
 # Create your views here.
+
+def index_view(request):
+    posts = Blogpost.objects.order_by('date')
+    context={'posts' : posts}
+
+    return render(request, 'blogs/index.html', context)
