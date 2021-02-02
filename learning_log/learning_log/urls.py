81035d30b72c18df04330c092f44dbca51d258b6
Jakub <rutrle@seznam.cz>
2021-02-03 00:00:00 +0100

    setting up app users

--- learning_log/learning_log/urls.py
+++ learning_log/learning_log/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls), 
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')), 
     path('', include('learning_logs.urls')),
 ]
