46aca7e5da2c65ee3db339f479a8178b9a20fa99
Jakub <rutrle@seznam.cz>
2021-02-10 20:29:42 +0100

    aded owning topics

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,7 +7,7 @@ class Topic(models.Model):
     "a topic which the user is learning about"
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True) #sets the attribute to time when user will add a date
-
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
         '''return string representation of model'''
